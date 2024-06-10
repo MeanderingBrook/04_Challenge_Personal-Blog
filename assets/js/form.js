d39a6acf4819c296970978b1ad7a6f1ca4fab3a4
Meandering Brook <jaguar_rebuke_0g@icloud.com>
2024-06-10 19:57:58 -0400

    Interim Form JS Update

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,23 +1,23 @@
 // Reference: https://blog.logrocket.com/localstorage-javascript-complete-guide/
 
 const ul = document.querySelector('ul');
-const input = document.getElementById('user-name')
+const input = document.getElementById('username')
 
 // Loads existing User Names from Local Storage to postsArray, else: empty Array
-let postsArray = localStorage.getItem('userNames') ? JSON.parse(localStorage.getItem('userNames')) : [];
+let postsArray = localStorage.getItem('usernames') ? JSON.parse(localStorage.getItem('usernames')) : [];
 
 // Creates <li> for each User Name, and adds to <ul>
-postsArray.forEach(addUserName);
-function addUserName(text) {
-  const li = document.createElement('li')
-  li.textContent = text;
-  ul.appendChild(li);
+postsArray.forEach(addPost);
+function addPost(text) {
+  const post = document.createElement('li')
+  post.textContent = text;
+  ul.appendChild(post);
 }
 
 function add() {
   postsArray.push(input.value);
-  localStorage.setItem('userNames', JSON.stringify(postsArray));
-  addUserName(input.value);
+  localStorage.setItem('usernames', JSON.stringify(postsArray));
+  addPost(input.value);
   input.value = '';
 }
 
